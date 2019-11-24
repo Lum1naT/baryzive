9eea493978623d40307c4d0c50134974c816c6c5
K0jnCZ <k0jncz@gmail.com>
2019-11-24 20:17:05 +0100

    started to work on my own backend framework
    love.
    -Lum1naT

---
+++ src/phpscripts/DatabaseManager.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once("../loginCredentials.php");
+
+$host = DB_HOST;
+$db   = DB_DATABASE;
+$user = DB_USERNAME;
+$pass = DB_PASSWORD;
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+ ?>
