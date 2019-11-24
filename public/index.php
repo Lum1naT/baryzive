4817baee8487eb1094f6eba31d0f5635117ad413
K0jnCZ <k0jncz@gmail.com>
2019-11-24 22:14:22 +0100

    magic!

--- public/index.php
+++ public/index.php
@@ -7,12 +7,12 @@ require __DIR__ . '/vendor/autoload.php';
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-$loader = new FilesystemLoader(__DIR__ . '/templates');
+$loader = new FilesystemLoader(__DIR__ . '/src/templates');
 $twig = new Environment($loader);
 
 echo $twig->render('first.html.twig', ['name' => 'John Doe',
     'occupation' => 'gardener']);
-    
+
 #echo $_SERVER['SERVER_NAME'];
 #echo $_SERVER['HTTP_HOST'];
 #echo $_SERVER['REQUEST_URI'];
