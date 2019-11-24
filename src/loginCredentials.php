b0e349aa4e5511d57cdf9a10a66915ecdde28a29
K0jnCZ <k0jncz@gmail.com>
2019-11-24 21:06:35 +0100

    Update loginCredentials.php

--- src/loginCredentials.php
+++ src/loginCredentials.php
@@ -1,4 +1,5 @@
 <?php
+echo "Login Credentials included succ.";
 define("DB_HOST", "baryzive.ciu3ar9isoci.eu-west-1.rds.amazonaws.com");
 define("DB_PORT", "5432");
 define("DB_USERNAME", "Luminat");
