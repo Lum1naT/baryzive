1425f0dff801663705cf5a584d17b2a526a1b748
K0jnCZ <k0jncz@gmail.com>
2019-11-24 22:04:44 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,18 @@
 <?php
 include_once("../src/phpscripts/DatabaseManager.php");
 echo "Hello World";
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$loader = new FilesystemLoader(__DIR__ . '/templates');
+$twig = new Environment($loader);
+
+echo $twig->render('first.html.twig', ['name' => 'John Doe',
+    'occupation' => 'gardener']);
+    
 #echo $_SERVER['SERVER_NAME'];
 #echo $_SERVER['HTTP_HOST'];
 #echo $_SERVER['REQUEST_URI'];
