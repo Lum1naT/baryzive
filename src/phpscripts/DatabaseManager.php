1f750f8df773dd515bb3f4381f5398eebe081e69
K0jnCZ <k0jncz@gmail.com>
2019-11-24 21:36:01 +0100

    Update DatabaseManager.php

--- src/phpscripts/DatabaseManager.php
+++ src/phpscripts/DatabaseManager.php
@@ -2,30 +2,30 @@
 echo "DB manager included successfuly.";
 
 define("DB_HOST", "baryzive.ciu3ar9isoci.eu-west-1.rds.amazonaws.com");
-define("DB_PORT", "5432");
 define("DB_USERNAME", "Luminat");
 define("DB_PASSWORD", "baryzive42");
 define("DB_DATABASE", "ebdb");
 
 $host = DB_HOST;
-$port = DB_PORT;
 $db   = DB_DATABASE;
-$user = DB_USERNAME;
-$pass = DB_PASSWORD;
+$username = DB_USERNAME;
+$password = DB_PASSWORD;
 $charset = 'utf8mb4';
 
 
+$dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
 
-$dsn = "pgsql:host=$host;dbname=$db";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-     echo "Connected successfuly.";
-} catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+try{
+ // create a PostgreSQL database connection
+ $conn = new PDO($dsn);
+
+ // display a message if connected to the PostgreSQL successfully
+ if($conn){
+ echo "Connected to the <strong>$db</strong> database successfully!";
+ }
+}catch (PDOException $e){
+ // report error message
+ echo $e->getMessage();
 }
+
  ?>
