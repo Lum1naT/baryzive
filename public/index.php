37bb4cbf4d27eeda7f0b621bf950fd3991ac936f
K0jnCZ <k0jncz@gmail.com>
2019-11-24 23:52:14 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -10,9 +10,13 @@ use Twig\Loader\FilesystemLoader;
 $loader = new FilesystemLoader(__DIR__ . '/src/templates');
 $twig = new Environment($loader);
 
-$stmt = $pdo->prepare('SELECT * FROM users WHERE first_name = ? AND lastname = ?');
-$stmt->execute(["wow.", "Pepa"]);
-$user = $stmt->fetch();
+
+$stmt = $pdo->query('SELECT * FROM users');
+foreach ($stmt as $row)
+{
+  echo $row['first_name'] . "\n";
+  echo $row['last_name'] . "\n";
+}
 
 #echo $_SERVER['SERVER_NAME'];
 #echo $_SERVER['HTTP_HOST'];
