c4cf1c281d31c3b1cb623e49281c73a206243be8
K0jnCZ <k0jncz@gmail.com>
2019-11-25 08:22:59 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,13 +2,7 @@
 include_once("../src/phpscripts/DatabaseManager.php");
 echo "Hello World";
 
-require __DIR__ . '/vendor/autoload.php';
 
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
-
-$loader = new FilesystemLoader(__DIR__ . '/src/templates');
-$twig = new Environment($loader);
 
 
 $stmt = $pdo->query('SELECT first_name, last_name FROM users');
