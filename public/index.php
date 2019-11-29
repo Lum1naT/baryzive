bb5724c7d71a62a920b9b7748eb9d4a0e7e3657d
K0jnCZ <k0jncz@gmail.com>
2019-11-30 00:40:09 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ use Twig\Loader\FilesystemLoader;
 include_once("../src/phpscripts/DatabaseManager.php");
 echo listAllUsers($pdo);
 
-echo createNewUser($pdo, "test_oauth_provider", "test_oauth_uid", "test_first_name", "test_last_name", "test_email", "test_gender", "test_locale", "test_link");
+echo createNewUser($pdo, "test_oauth_provider", "test_oauth_uid", "David", "Vítek", "K0jnCZ@gmail.com", "M", "cs_CZ", "test_link");
 
 
 $loader = new FilesystemLoader('../src/templates');
