a9bab17406094d823faabd6928eabeb106e9b9eb
K0jnCZ <k0jncz@gmail.com>
2019-11-30 01:00:59 +0100

    Update DatabaseManager.php

--- src/phpscripts/DatabaseManager.php
+++ src/phpscripts/DatabaseManager.php
@@ -49,17 +49,10 @@ function listAllUsers($pdoInstance){
 
 
  function createNewUser($pdoInstance, $oauth_provider, $oauth_uid, $first_name, $last_name, $email, $gender, $locale, $link){
-
-   try {
-    $pdoInstance->beginTransaction();
-    $stmt = $pdoInstance->prepare("INSERT INTO users (oauth_provider, oauth_uid, first_name, last_name, email, gender, locale, link) VALUES (?,?,?,?,?,?,?,?)");
-        $stmt->execute([$oauth_provider, $oauth_uid, $first_name, $last_name, $email, $gender, $locale, $link]);
-
-    $pdoInstance->commit();
-}catch (Exception $e){
-    $pdoInstance->rollback();
-    throw $e;
-}
+   $sql = "INSERT INTO users (oauth_provider, oauth_uid, first_name, last_name, email, gender, locale, link) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+   $stmt = $pdoInstance->prepare($sql);
+   $stmt->execute([$oauth_provider, $oauth_uid, $first_name, $last_name, $email, $gender, $locale, $link]);
+  
 
  }
 
