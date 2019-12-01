3799c262653afe1d4147040dbe52198385672238
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2019-12-01 16:52:52 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,23 +8,24 @@ use Twig\Loader\FilesystemLoader;
 include_once("../src/phpscripts/DatabaseManager.php");
 
 
-
-
-try {
-  $stmt = $pdo->prepare("INSERT INTO users (oauth_provider, oauth_uid, first_name, last_name, email, gender, locale, link, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-
-    $stmt->execute(["intern", "42069", "David", "Vítek", "K0jnCZ@gmail.com", "M", "cz_cs",  "customlink", "1"]);
-    echo "Inserted successfully.";
-}catch (PDOException $e){
-    throw $e;
-}
-
 $loader = new FilesystemLoader('../src/templates');
 $twig = new Environment($loader);
 
 echo $twig->render('base.html.twig', ['name' => 'John Doe',
     'occupation' => 'gardener']);
 
+
+
+
+
+    try {
+      $stmt = $pdo->prepare("INSERT INTO users (oauth_provider, oauth_uid, first_name, last_name, email, gender, locale, link, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+        $stmt->execute(["intern", "42069", "David", "Vítek", "K0jnCZ@gmail.com", "M", "cz_cs",  "customlink", "1"]);
+        echo "Inserted successfully.";
+    }catch (PDOException $e){
+        throw $e;
+    }
 
 /*
 $stmt = $conn->query('SELECT first_name, last_name FROM users');
