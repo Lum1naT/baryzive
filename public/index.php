9972224b6c55010b822e909540516e8e0b69ff22
K0jnCZ <k0jncz@gmail.com>
2019-12-17 12:18:09 +0100

    routing

--- public/index.php
+++ public/index.php
@@ -18,17 +18,23 @@ echo $twig->render('base.html.twig', ['name' => 'John Doe',
     $request = $_SERVER['REQUEST_URI'];
     switch ($request) {
     case '/' :
-    echo "/index.php";
-      //  require __DIR__ . '/views/index.php';
+    echo $twig->render('base.html.twig', ['name' => 'John Doe',
+        'occupation' => 'gardener']);
         break;
+
     case '' :
-    echo " index.php";
-      //  require __DIR__ . '/views/index.php';
+    echo $twig->render('base.html.twig', ['name' => 'John Doe',
+        'occupation' => 'gardener']);
         break;
+
+    case '/about' :
+    echo $twig->render('register.html.twig', ['name' => 'John Doe',
+        'occupation' => 'gardener']);
     default:
         http_response_code(404);
-        require __DIR__ . '/views/404.php';
-        break;
+        echo $twig->render('base.html.twig', ['name' => 'John Doe',
+            'occupation' => 'gardener']);
+            break;
 }
 /*
 $stmt = $conn->query('SELECT first_name, last_name FROM users');
