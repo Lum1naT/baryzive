fabbc8527d99991ed5ed7e70f9efd3ef9ada3529
K0jnCZ <k0jncz@gmail.com>
2019-12-17 12:09:11 +0100

    ok

--- public/index.php
+++ public/index.php
@@ -15,7 +15,21 @@ $twig = new Environment($loader);
 echo $twig->render('base.html.twig', ['name' => 'John Doe',
     'occupation' => 'gardener']);
 
-
+    $request = $_SERVER['REQUEST_URI'];
+    switch ($request) {
+    case '/' :
+    echo "/index.php"
+      //  require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+    echo " index.php";
+      //  require __DIR__ . '/views/index.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
 /*
 $stmt = $conn->query('SELECT first_name, last_name FROM users');
 foreach ($stmt as $row)
