28d05dd9cdca05a006cafb851faae16e3fc6a93f
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2019-12-18 21:56:55 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -41,7 +41,7 @@ Route::add('/contact-form',function(){
 },'post');
 
 // Accept only numbers as parameter. Other characters will result in a 404 error
-Route::add('/foo/([0-9]*)/bar',function($var1){
+Route::add('/bar/([A-Z]*[a-z]*)\w+)',function($var1){
     echo $var1.' is a great number!';
 });
 
