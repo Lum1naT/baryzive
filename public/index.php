f552bc7223a2da03aee33d760f08d328e6837c15
K0jnCZ <k0jncz@gmail.com>
2019-12-19 09:50:09 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -48,7 +48,7 @@ Route::add('/contact-form',function(){
 * @param $cameraname (string, unique)
 *
 */
-Route::add('/bar/([A-Z]*[a-z]*[0-9]*)/([A-Z]*[a-z]*[0-9]*)',function($barName, $cameraName){
+Route::add('/bar/([a-z]*[0-9]*)\S/([a-z]*[0-9]*)\S',function($barName, $cameraName){
   // TODO: check if bar is in DB
 
     echo $barName.' is a great bar! <br/> You are viewing '. $cameraName . ' camera.' ;
