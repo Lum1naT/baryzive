c6b64bfb2e86128385570971d6920a2828afbd87
dsetty <50793798+dsetty@users.noreply.github.com>
2020-01-01 19:41:35 +0100

    dark theme script

--- public/assets/scripts/swipe-btn.js
+++ public/assets/scripts/swipe-btn.js
@@ -1,6 +1,25 @@
 $(document).ready (function(){
   var body = $('body');
   var swipeBtn = $('#swipe-checkbox');
+  var swipeBtnValue = JSON.parse(localStorage.getItem('swipeBtnValue')) || {},
+      $checkbox = $(".swipe-btn :checkbox");
+
+  $checkbox.on("change", function(){
+    $checkbox.each(function(){
+      swipeBtnValue[this.id] = this.checked;
+    });
+
+    localStorage.setItem("swipeBtnValue", JSON.stringify(swipeBtnValue));
+  });
+
+  // On page load
+  $.each(swipeBtnValue, function(key, value) {
+    $("#" + key).prop('checked', value);
+  });
+
+  if(swipeBtn.is(':checked')){
+    body.attr('id', 'dark-theme');
+  }
 
   swipeBtn.click(function(){
     if(swipeBtn.is(':checked')){
