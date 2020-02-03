b50300c9f116b489109da5ef1d0fab11752eec02
K0jnCZ <k0jncz@gmail.com>
2020-02-03 15:15:17 +0100

    Update DatabaseManager.php

--- public/assets/phpscripts/DatabaseManager.php
+++ public/assets/phpscripts/DatabaseManager.php
@@ -1,5 +1,28 @@
 <?php
+/*
 
+Some dummy syntax incase this link is not avalible
+https://phpdelusions.net/pdo_examples/
+https://phpdelusions.net/
+
+$sql = "UPDATE users SET name=?, surname=?, sex=? WHERE id=?";
+$stmt= $pdo->prepare($sql)->execute([$name, $surname, $sex, $id]);
+
+$data = $pdo->query("SELECT * FROM users")->fetchAll();
+// and somewhere later:
+foreach ($data as $row) {
+   echo $row['name']."<br />\n";
+}
+
+$stmt = $pdo->prepare("SELECT * FROM users WHERE id=?");
+$stmt->execute([$id]);
+$user = $stmt->fetch();
+
+$sql = "INSERT INTO users (name, surname, sex) VALUES (?,?,?)";
+$stmt= $pdo->prepare($sql);
+$stmt->execute([$name, $surname, $sex]);
+
+*/
 define("DB_HOST", "baryzive.ciu3ar9isoci.eu-west-1.rds.amazonaws.com");
 define("DB_USERNAME", "Luminat");
 define("DB_PASSWORD", "baryzive42");
@@ -28,26 +51,10 @@ try{
 }
 
 
-function listAllUsers($pdoInstance){
-  $stmt = $pdoInstance->query('SELECT * FROM users');
-  foreach ($stmt as $row)
-  {
-    echo $row['id'] . "\n";
-    echo $row['oauth_provider'] . "\n";
-    echo $row['oauth_uid'] . "\n";
-    echo $row['email'] . "\n";
-    echo $row['first_name'] . "\n";
-    echo $row['last_name'] . "\n";
-    echo $row['gender'] . "\n";
-    echo $row['locale'] . "\n";
-    echo $row['link'] . "\n";
-    echo $row['created'] . "\n";
-    echo $row['modified'] . "\n";
-  }
- }
+
 
  function authenticateUser($pdoInstance, $email, $authenticationCode){
-   // TODO: check if $email and $authenticationCode match
+   // TODO: check if $email and $authenticationCode match > done mby?
    $stmt = $pdoInstance->query('SELECT authentication_code FROM users WHERE email = ?');
    $stmt->execute([$email]);
    $user = $stmt->fetch();
