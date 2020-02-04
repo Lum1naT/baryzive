89ca329cf3dff3ac23f6392803c0123a58bfaa3a
K0jnCZ <k0jncz@gmail.com>
2020-02-04 15:20:47 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -132,7 +132,23 @@ Route::add('/fblogin',function(){
   $loginUrl = $helper->getLoginUrl('https://dom.baryzive.cz/fb-callback.php', $permissions);
 
   echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
-}, 'get');
+},'get');
+
+Route::add('/fblogin',function(){
+  $fb = new Facebook\Facebook([
+    'app_id' => '752564898557063', // Replace {app-id} with your app id
+    'app_secret' => 'e533ba2fc0fd1c72d72ebbb8c8feb8d5',
+    'default_graph_version' => 'v3.2',
+    ]);
+
+  $helper = $fb->getRedirectLoginHelper();
+
+  $permissions = ['email']; // Optional permissions
+  $loginUrl = $helper->getLoginUrl('https://dom.baryzive.cz/fb-callback.php', $permissions);
+
+  echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+});
+
 /*
 * @param $barname (string, unique)
 * @param $cameraname (string, unique)
